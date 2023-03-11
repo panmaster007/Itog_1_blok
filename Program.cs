4638b53e5ceb4edd45b7de5784750ea7bc7dbfe1
Igor <fullstack@naladka.su>
2023-03-11 12:41:50 +0300

    edit function

--- Program.cs
+++ Program.cs
@@ -1,41 +1,53 @@
 ﻿// Функция принимает число из консоли и обходит ошибку ввода
-int IntInput(string intName) // функция принимает число из консоли, преобразуя его в целое                   
+int IntInput(string argument) // функция принимает число из консоли, преобразуя его в целое                   
 {
-    int num;
+	int num;
 
-    Console.Write($"Введите число {intName}: ");
-    while(!int.TryParse(Console.ReadLine(), out num))
-    {
-        Console.WriteLine("Ерунда какая-то... Ощущение, что введено не целое число.");
-        Console.Write($"Ну-ка, еще разок! {intName} = ");
-    }
-    return  num;
+	Console.Write($"Введите число {argument}: ");
+	while (!int.TryParse(Console.ReadLine(), out num))
+	{
+		Console.WriteLine("Error! it's not an integer!");
+		Console.Write($"Try again! {argument} = ");
+	}
+	return num;
 }
 
 // Функция создает массив целых элементов, введенных пользователем
 int[] createArr(int size)
 {
-    int[] arr = new int[size];
-    for (int i=0; i < size; i++)
-    {
-        arr[i] = IntInput("arr[" + i + "]");
-        //Console.WriteLine($"arr[{i}] = {arr[i]}");
-        //Console.WriteLine(string.Join(" ",arr));
-    }
-    return arr;
+	int[] arr = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = IntInput("arr[" + i + "]");
+		//Console.WriteLine($"arr[{i}] = {arr[i]}");
+		//Console.WriteLine(string.Join(" ",arr));
+	}
+	return arr;
 }
 
-// Функция распечатывает содержимое массива
-void printArr(int[] array)
+// Функция распечатывает содержимое строчного массива через запятую кроме последнего элемента
+void PrintArr(string[] array)
 {
-    string str = "";
-    for (int i = 0; i <= array.Length-1; i++)
-    {
-        if (i != array.Length-1 )
-        {
-            str = str  + array[i] + ", ";
-        }
-        else str = str  + array[i] + ".";
-    }
-    Console.WriteLine(str);
+	for (int i = 0; i <= array.Length - 1; i++)
+	{
+		if (i == array.Length - 1)
+		{
+			Console.WriteLine($"{array[i]}");
+		}
+		else
+		{
+			Console.WriteLine($"{array[i]}, ");
+		}
+	}
 }
+
+// Вызов и исполнение модулей(методов) программы
+Console.WriteLine();
+int maxLength = 4;
+
+string[] array = GetFilledArrayOfString(IntInput());
+PrintArr(array);
+
+string[] arr = GetArrayOfStringLessThanLength(maxLength, array);
+PrintArr(arr);
+Console.WriteLine();
